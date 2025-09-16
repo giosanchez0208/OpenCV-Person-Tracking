13fac78b79e94d4dbbe8961ccdf80b28179bb9ab
Gio Sanchez <giokiefersanchez@gmail.com>
2025-09-16 12:19:29 +0800

    fix: Save Next Frame for Processing

--- python/tracking_pipeline.py
+++ python/tracking_pipeline.py
@@ -3,16 +3,19 @@ import cv2
 curr_frame = None
 
 def tracking_pipeline(next_frame):
+    
     global curr_frame
+    
+    # no incoming or outgoing frame
     if next_frame is None:
         return None
-
-    # Initialize baseline on first call
     if curr_frame is None:
         curr_frame = next_frame.copy().astype("float32")
         return next_frame
 
-    # Subtract baseline from current next_frame
+    # actual pipeline with curr and next frame
     diff = cv2.absdiff(next_frame, curr_frame.astype("uint8"))
 
+    # update current frame
+    curr_frame = next_frame.copy().astype("float32")
     return diff
