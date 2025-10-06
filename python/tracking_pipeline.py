835abacf7627c64c99d01d1c664d7fb06c3a1912
giosanchez0208 <giokiefersanchez@gmail.com>
2025-10-06 13:27:53 +0800

    feat: Single Object Annotationn

--- python/tracking_pipeline.py
+++ python/tracking_pipeline.py
@@ -1,17 +1,33 @@
+from dataclasses import dataclass
 import cv2
-from python.detect import *
+from python.detect import detect_people_bboxes
+from python.annotate import annotate_bbox
 
-# Global variables
-curr_frame = None
+@dataclass
+class Tracker:
+    curr_frame = None
+    curr_bboxes: list = None
+
+tracker = Tracker()  # make tracker global
 
 def tracking_pipeline(next_frame):
     global tracker
-    global curr_frame
 
     if next_frame is None:
         return None
 
-    # Update current frame
-    curr_frame = next_frame.copy()
-    
-    return next_frame
+    if tracker.curr_frame is None:
+        tracker.curr_frame = next_frame.copy()
+        tracker.curr_bboxes = detect_people_bboxes(next_frame)
+        return next_frame
+
+    next_bboxes = detect_people_bboxes(next_frame)
+    final_frame = next_frame.copy()
+
+    for bbox in next_bboxes:
+        final_frame = annotate_bbox(final_frame, bbox, label="New")
+
+    tracker.curr_frame = next_frame.copy()
+    tracker.curr_bboxes = next_bboxes
+
+    return final_frame
